5acf81eb3c2da0a80ee6d08ab6c6336f9c407451
SorayaYF <sorayayasin@hotmail.com>
2023-07-17 13:18:53 -0300

    Update table

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { FormComponent } from './components/form/form.component';
 import { TableComponent } from './components/table/table.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 
-
-
 @NgModule({
-  declarations: [
-    HomeComponent,
-    FormComponent,
-    TableComponent
-  ],
+  declarations: [HomeComponent, FormComponent, TableComponent],
   imports: [CommonModule, FormsModule, HttpClientModule],
   exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
